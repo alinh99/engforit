8012d2050a460167549ad06536217761f23899e4
alinh99 <alinh1803@gmail.com>
2022-08-24 10:02:32 +0700

    setting screen done

---
+++ lib/Screens/settings/components/setting_icon_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SettingIconButton extends StatelessWidget {
+  const SettingIconButton({
+    Key key,
+    this.iconButton,
+    this.titleButton,
+    this.widthSize,
+  }) : super(key: key);
+  final IconData iconButton;
+  final String titleButton;
+  final double widthSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 24),
+          padding: const EdgeInsets.all(8),
+          decoration: const BoxDecoration(
+            color: Color(0xffF6F7FE),
+            //color: Colors.red,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          child: Icon(
+            iconButton,
+            color: const Color(0xff112851),
+          ),
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        Text(titleButton),
+        SizedBox(
+          width: widthSize,
+        ),
+        const Icon(Icons.keyboard_arrow_right)
+      ],
+    );
+  }
+}
--- lib/Screens/settings/settings.dart
+++ lib/Screens/settings/settings.dart
@@ -1,3 +1,4 @@
+import 'package:eft_project/Screens/settings/components/setting_icon_button.dart';
 import 'package:eft_project/components/app_bar.dart';
 import 'package:eft_project/components/bottom_navigation_bar.dart';
 import 'package:eft_project/enum.dart';
@@ -14,9 +15,122 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: CustomBottomNavBar(selected: MenuState.profile),
+      bottomNavigationBar:
+          const CustomBottomNavBar(selected: MenuState.profile),
       body: Appbar(
         title: 'Settings',
+        body: SafeArea(
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(left: 24, top: 16),
+                        child: const CircleAvatar(
+                          radius: 40,
+                          backgroundColor: Color(0xffd9dffd),
+                          backgroundImage:
+                              AssetImage('assets/images/avatar.jpg'),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(top: 16),
+                            child: const Text(
+                              'Nguyen Thi My Duyen',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          const Text(
+                            '22',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Divider(
+                    height: 40,
+                    indent: 24,
+                    endIndent: 24,
+                  ),
+                  const SettingIconButton(
+                    iconButton: Icons.person,
+                    titleButton: 'Personal Data',
+                    widthSize: 144,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const SettingIconButton(
+                    iconButton: Icons.settings,
+                    titleButton: 'Settings',
+                    widthSize: 180,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const SettingIconButton(
+                    iconButton: Icons.notifications,
+                    titleButton: 'Notifications',
+                    widthSize: 152,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const SettingIconButton(
+                    iconButton: Icons.live_help_rounded,
+                    titleButton: 'Help Center',
+                    widthSize: 158,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const SettingIconButton(
+                    iconButton: Icons.logout,
+                    titleButton: 'Log out',
+                    widthSize: 184,
+                  ),
+                  const Divider(
+                    height: 40,
+                    indent: 24,
+                    endIndent: 24,
+                  ),
+                  const SettingIconButton(
+                    iconButton: Icons.question_answer_outlined,
+                    titleButton: 'FAQs',
+                    widthSize: 198,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const SettingIconButton(
+                    iconButton: Icons.people_rounded,
+                    titleButton: 'Community',
+                    widthSize: 158,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
